5889f70ca0bdd095bc3fd525cd54028f8f3c48c1
HenryJKS <henryjooji30@gmail.com>
2024-01-28 14:39:03 -0300

    vite

---
+++ henryjks-forum/src/components/Counter.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    data: () => ({
+        count: 10,
+        counterTitle: "Counter Standard",
+        incrementAmount: 8
+    }),
+    computed: {
+        displayTile() {
+            if (this.count > 20) {
+                return "Counter Standard - Very Long" 
+            } else {
+                return "Counter Standard"
+            }
+        },
+        optimizedIncrementAmount() {
+            return this.displayTile.length * this.incrementAmount
+        }
+    },
+    methods: {
+        incrementCount(newAmount, event) {
+            console.log(newAmount)
+            console.log(event)
+            this.count += this.optimizedIncrementAmount
+        }
+    }
+}
+</script>
+
+<template>
+    <h2>{{ displayTile }}</h2>
+    <p :data-increment-by="incrementAmount">{{ incrementAmount }}</p>
+    <button @click="incrementCount">Increment Count</button>
+    <h1>{{ incrementAmount }}</h1>
+    <p>{{  optimizedIncrementAmount }}</p>
+    <div>
+        <label for="incrementAmount">Increment By:</label>
+        <input type="text" v-model="incrementAmount">
+    </div>
+</template>
